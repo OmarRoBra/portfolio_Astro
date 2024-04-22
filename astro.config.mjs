29bd509c9c090ad408f2eaafb1f8e1f1cd8707fd
Jesus Omar <jesusomar@Jesuss-Mac-mini.local>
2024-04-22 09:08:28 -0600

    fix: fix netlify errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "hybrid", // JUST ADD THIS AND IT ALL WORKS! YAY!
+  output: "server",
   adapter: netlify(),
 });
